5137bfb43bd469c24c8896615ab50c4575a47446
rverma-dev <rverma.dev@gmail>
2023-12-15 13:42:27 +0530

    updated commands

--- src/Mil.cpp
+++ src/Mil.cpp
@@ -14,7 +14,7 @@
 #pragma comment(lib, "ws2_32.lib") // Link with ws2_32.lib
 #pragma comment(lib, "McxAPI")
 
-#define PORT 8888
+#define PORT 5505
 #define DeviceId 0
 #define nRtNo 0x20 // rt 5 -> 2^5//
 
@@ -24,14 +24,14 @@
 #define ICD_6_4 3
 
 // cmd createion RT<<11|Tx/Rx<<10|SA<<5|WC
-#define cmd_6_1_tx 0x2E1F // 5-T-16-32
-#define cmd_6_1_rx 0x1DF0 // 3-R-6-32
-#define cmd_6_2_tx 0x2E83 // 5-T-20-4
-#define cmd_6_2_rx 0x1883 // 3-R-4-4
-#define cmd_6_3_tx 0x2F7F // 5-T-27-32
-#define cmd_6_3_rx 0x193F // 3-R-9-32
-#define cmd_6_4_tx 0x2C4F // 5-T-2-16
-#define cmd_6_4_rx 0x199F // 3-R-21-16
+#define cmd_6_1_tx 0x2E00 // 5-T-16-32
+#define cmd_6_1_rx 0x18C0 // 3-R-6-32
+#define cmd_6_2_tx 0x2E84 // 5-T-20-4
+#define cmd_6_2_rx 0x1884 // 3-R-4-4
+#define cmd_6_3_tx 0x2F60 // 5-T-27-32
+#define cmd_6_3_rx 0x1920 // 3-R-9-32
+#define cmd_6_4_tx 0x2C40 // 5-T-2-16
+#define cmd_6_4_rx 0x1AB0 // 3-R-21-16
 // UINT16 cmd1 = 0x2E00; // 5-T-16-32
 // UINT16 cmd1r = 0x18C0;// 3-R-6-32
 
@@ -41,11 +41,12 @@ static UINT16 DB3 = 2;
 static UINT16 DB4 = 3;
 static UINT16 BusList1 = 0;
 
-UINT16 datablock_6_1[64]{};
-UINT16 datablock_6_2[64]{};
-UINT16 datablock_6_3[64]{};
-UINT16 datablock_6_4[64]{};
+UINT16 datablock_6_1[32]{};
+UINT16 datablock_6_2[4]{};
+UINT16 datablock_6_3[32]{};
+UINT16 datablock_6_4[16]{};
 char errorCode[1000];
+U8BIT transmitted[4];
 
 INT16 InitDevice() {
   INT16 res = 0;
@@ -93,16 +94,16 @@ INT16 CreateBusFrame() {
 
   //////////////////////////////////////////////////////////////////////////
   // only for RT-RT
-  iResult = mcx_Create_BusList_Element(DeviceId, ICD_6_1, cmd_6_1_tx, 0x80 | messageOptions, cmd_6_1_rx, rxStt, txStt);
+  iResult = mcx_Create_BusList_Element(DeviceId, ICD_6_1, cmd_6_1_rx, 0x80 | messageOptions, cmd_6_1_tx, rxStt, txStt);
   if (iResult < 0)
     return iResult;
-  iResult = mcx_Create_BusList_Element(DeviceId, ICD_6_2, cmd_6_2_tx, 0x80 | messageOptions, cmd_6_2_rx, rxStt, txStt);
+  iResult = mcx_Create_BusList_Element(DeviceId, ICD_6_2, cmd_6_2_rx, 0x80 | messageOptions, cmd_6_2_tx, rxStt, txStt);
   if (iResult < 0)
     return iResult;
-  iResult = mcx_Create_BusList_Element(DeviceId, ICD_6_3, cmd_6_3_tx, 0x80 | messageOptions, cmd_6_3_rx, rxStt, txStt);
+  iResult = mcx_Create_BusList_Element(DeviceId, ICD_6_3, cmd_6_3_rx, 0x80 | messageOptions, cmd_6_3_tx, rxStt, txStt);
   if (iResult < 0)
     return iResult;
-  iResult = mcx_Create_BusList_Element(DeviceId, ICD_6_4, cmd_6_4_tx, 0x80 | messageOptions, cmd_6_4_rx, rxStt, txStt);
+  iResult = mcx_Create_BusList_Element(DeviceId, ICD_6_4, cmd_6_4_rx, 0x80 | messageOptions, cmd_6_4_tx, rxStt, txStt);
   if (iResult < 0)
     return iResult;
   // For RT to BC
@@ -111,16 +112,16 @@ INT16 CreateBusFrame() {
 
   //////////////////////////////////////////////////////////////////////////
   // To create element data block
-  iResult = mcx_Create_Element_DataBlock(DeviceId, DB1, 0, datablock_6_1, 64);
+  iResult = mcx_Create_Element_DataBlock(DeviceId, DB1, 0, datablock_6_1, 32);
   if (iResult < 0)
     return iResult;
-  iResult = mcx_Create_Element_DataBlock(DeviceId, DB2, 0, datablock_6_2, 64);
+  iResult = mcx_Create_Element_DataBlock(DeviceId, DB2, 0, datablock_6_2, 4);
   if (iResult < 0)
     return iResult;
-  iResult = mcx_Create_Element_DataBlock(DeviceId, DB3, 0, datablock_6_3, 64);
+  iResult = mcx_Create_Element_DataBlock(DeviceId, DB3, 0, datablock_6_3, 32);
   if (iResult < 0)
     return iResult;
-  iResult = mcx_Create_Element_DataBlock(DeviceId, DB4, 0, datablock_6_4, 64);
+  iResult = mcx_Create_Element_DataBlock(DeviceId, DB4, 0, datablock_6_4, 16);
   if (iResult < 0)
     return iResult;
   /////////////////////////////////////////////////////////////////////////////
@@ -174,43 +175,52 @@ void Process(const std::string dataString) {
   ParseFGMessage(dataString, data1, data2, data3, data4);
 
   INT16 iResult = 0;
-  iResult = SendMessage_6_1(data1);
+  iResult = mcx_Get_Buslist_TransmittedElements(DeviceId, BusList1, transmitted);
   if (iResult < 0) {
     mcx_GetReturnCodeDescription(iResult, errorCode);
     printf("Error -> %s\n", errorCode);
     return;
   }
-  Sleep(2);
 
-  iResult = SendMessage_6_2(data2);
-  if (iResult < 0) {
-    mcx_GetReturnCodeDescription(iResult, errorCode);
-    printf("Error -> %s\n", errorCode);
-    return;
-  }
-  Sleep(2);
+  if (transmitted[0]) {
 
-  iResult = SendMessage_6_3(data3);
-  if (iResult < 0) {
-    mcx_GetReturnCodeDescription(iResult, errorCode);
-    printf("Error -> %s\n", errorCode);
-    return;
+    iResult = SendMessage_6_1(data1);
+    if (iResult < 0) {
+      mcx_GetReturnCodeDescription(iResult, errorCode);
+      printf("Error -> %s\n", errorCode);
+      return;
+    }
   }
-  Sleep(2);
-
-  iResult = SendMessage_6_4(data4);
-  if (iResult < 0) {
-    mcx_GetReturnCodeDescription(iResult, errorCode);
-    printf("Error -> %s\n", errorCode);
-    return;
+  if (transmitted[1]) {
+    iResult = SendMessage_6_2(data2);
+    if (iResult < 0) {
+      mcx_GetReturnCodeDescription(iResult, errorCode);
+      printf("Error -> %s\n", errorCode);
+      return;
+    }
   }
-  Sleep(2);
+  if (transmitted[2]) {
+    iResult = SendMessage_6_3(data3);
+    if (iResult < 0) {
+      mcx_GetReturnCodeDescription(iResult, errorCode);
+      printf("Error -> %s\n", errorCode);
+      return;
+    }
+  }
+  if (transmitted[3]) {
+    iResult = SendMessage_6_4(data4);
+    if (iResult < 0) {
+      mcx_GetReturnCodeDescription(iResult, errorCode);
+      printf("Error -> %s\n", errorCode);
+      return;
+    }
+  }
+  Sleep(1);
 }
 
 INT16 SendMessage_6_1(ICD_6_1_data data) {
   INT16 results = 0;
   prepare(data, datablock_6_1);
-  // result = mcx_Element_DataBlock_Write(DeviceId, ICD_6_2, DB2, buff_I21, 32);
   hexPrint(datablock_6_1, 32);
   results = mcx_Element_UpdateData(DeviceId, BusList1, ICD_6_1);
   return results;
@@ -258,22 +268,25 @@ static inline void hexPrint(UINT16 buffer[], int length) {
 
 int main() {
 
-  Byte buffer[512];
+  Byte buffer[1024];
   Int32 size;
   Int32 cnt;
 
-    UDPLanInterface sockObj(0, PORT, (Byte*)"127.0.0.1", RX_MODE);
-    // Initialize device
-    InitDevice();
+  UDPLanInterface sockObj(0, PORT, (Byte *)"127.0.0.1", RX_MODE);
+  // Initialize device
+  InitDevice();
 
   while (1) {
-    if ((cnt = sockObj.RecvPack(buffer, &size)) > 0) {
-      std::string dataString(buffer);
-      buffer[cnt] = '\0';
-      rtrim(dataString);
-      std::cout << "Processing data: " << dataString << std::endl;
-      Process(dataString);
-    }
+    Process("0.756 ,640.9234 ,-904.1187 ,13.43551 ,305.4992 ,0.008158886 ,-1.043156 ,5.331967 ,306.6131 ,77.68379 "
+            ",3047.88  ,12.86905 ,-3.593511e-05 ,-0.03492117 ,0.0007221791 ,309.9201 ,19.68432 ,656.619  ,2023 ,12   "
+            ",13  ,  22 ,58   ,40   ,378");
+    // if ((cnt = sockObj.RecvPack(buffer, &size)) > 0) {
+    //   std::string dataString(buffer);
+    //   buffer[cnt] = '\0';
+    //   rtrim(dataString);
+    //   std::cout << "Processing data: " << dataString << std::endl;
+    //   // Process(dataString);
+    // }
   }
 
   mcx_Free(DeviceId);
